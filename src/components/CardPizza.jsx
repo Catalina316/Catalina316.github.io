38a5821eb73289fe9c116fc5d5d6b67b03107665
Catalina316 <za.catalina@gmail.com>
2024-11-15 21:18:28 -0300

    Cambio ngredientes

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -8,7 +8,7 @@ const CardPizza = (props) => {
       <Card.Img variant="top" src={props.imagen} />
       <Card.Body>
         <Card.Title>{props.nombre}</Card.Title>
-        <Card.Text>Ingredientes: {props.descripcion}</Card.Text>
+        <Card.Text>Ingredientes varios: {props.descripcion}</Card.Text>
         <Card.Text>Precio: ${formateador(props.precio)}</Card.Text>
         <Button variant="primary">Ver más</Button>
         <Button variant="secondary">Añadir</Button>
